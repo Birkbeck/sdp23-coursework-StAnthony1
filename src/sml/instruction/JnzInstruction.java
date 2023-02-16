03d33296a1351aac4b5a0b8524fb91d32d9f5f3f
Oz O'Neill <oswald.oneill@gmail.com>
2023-02-16 14:54:54 +0000

    JnzInstruction complete with exception of toString(), hashCode(), and equals(). JnzInstructionTest working, including NullPointerException for labels which don't exist in the Machine's Labels Field.

---
+++ src/sml/instruction/JnzInstruction.java
@@ -0,0 +1,45 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+public class JnzInstruction extends Instruction {
+
+    private final RegisterName result;
+
+    private final String prior_labelA;
+
+    public static final String OP_CODE = "jnz";
+
+    public JnzInstruction(String label, RegisterName result, String prior_label) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.prior_labelA = prior_label;
+
+    }
+
+    @Override
+    public int execute(Machine m) throws NullPointerException{
+        int value = m.getRegisters().get(result);
+        if (value == 0)
+            return m.getLabels().getAddress(prior_labelA);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+    @Override
+    //TODO: implement method
+    public boolean equals(Object o) {
+        return true;
+    }
+
+    @Override
+    //TODO: implement method
+    public int hashCode(){
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + " " + prior_labelA;
+    }
+}
---
+++ src/sml/tests/JnzInstructionTest.java
@@ -0,0 +1,54 @@
+package sml.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+import sml.instruction.JnzInstruction;
+
+import static sml.Registers.Register.EAX;
+
+public class JnzInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+    //TODO create HashMap<String, Integer> or Lebels Object and populate for testing -
+    // do it via Machine instantiation or manually for testing purposes.
+    @Test
+    void executeLoopRegisterIsZero() {
+        registers.set(EAX, 0);
+        machine.getLabels().addLabel("f3", 2);
+        Instruction instruction = new JnzInstruction(null, EAX, "f3");
+        Assertions.assertEquals(2, instruction.execute(machine));
+    }
+
+    @Test
+    void executeSkipLoopRegisterNotZero() {
+        machine.getLabels().addLabel("f3", 2);
+        registers.set(EAX, 1);
+        Instruction instruction = new JnzInstruction(null, EAX, "f3");
+        Assertions.assertEquals(-1, instruction.execute(machine));
+    }
+
+    @Test
+    void invalidLabel(){
+        registers.set(EAX, 0);
+        Instruction instruction = new JnzInstruction(null, EAX, "f3");
+        Assertions.assertThrows(NullPointerException.class ,()-> instruction.execute(machine));
+    }
+}
